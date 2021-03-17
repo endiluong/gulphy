97c091ab27402d86614ca02d82339cb8c90de467
Duy Luong <desmonmiles8996@gmail.com>
2021-03-17 09:49:09 +0700

    Ali Hunter News Header and Nav

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  semi: true,
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  // 'liveServer.settings.port': 5501
+};
